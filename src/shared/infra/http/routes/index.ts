c53485dcf27d5513001fc5f5eea87313859a1014
ErildoJS <erildofranciscojs@gmail.com>
2024-01-20 15:44:48 +0100

    add rentals and cars routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,13 +4,15 @@ import { specificationRoutes } from "./specification.routes";
 import { usersRoutes } from "./users.routes";
 import { authenticateRoutes } from "./authenticate.routes";
 import { rentalRoutes } from "./rental.routes";
+import { carsRoutes } from "./cars.routes";
 
 const router = Router()
 
 router.use("/categories", categoriesRoutes)
 router.use("/specifications", specificationRoutes)
 router.use("/users", usersRoutes)
-router.use(authenticateRoutes)
+router.use("/cars", carsRoutes)
 router.use("/rentals", rentalRoutes)
+router.use(authenticateRoutes)
 
 export {router}

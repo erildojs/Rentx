7e0d6ff64ff23c48717e81c6c0ed4990e645cdad
ErildoJS <erildofranciscojs@gmail.com>
2024-01-25 12:27:06 +0100

    fix

--- src/modules/cars/usecases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/usecases/createCategory/CreateCategoryController.spec.ts
@@ -24,7 +24,8 @@ describe('Create Category Controller', () => {
       await connection.dropDatabase()
       await connection.close()
     })
-    it('should be able to create a new category', async() => {
+    
+    it('should be able to create a new category', async () => {
       const responseToken = await request(app).post('/sessions').send({
         email: 'admin@rentx.com',
         password: 'admin'
@@ -36,7 +37,8 @@ describe('Create Category Controller', () => {
       }).set({Authorization: `Bearer ${token}`})
       expect(response.status).toBe(201)
     })
-    it('should not be able to create a new category with same name ', async() => {
+
+    it('should not be able to create a new category with same name ', async () => {
       const responseToken = await request(app).post('/sessions').send({
         email: 'admin@rentx.com',
         password: 'admin'

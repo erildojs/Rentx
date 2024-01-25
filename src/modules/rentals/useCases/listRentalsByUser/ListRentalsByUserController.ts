2ebc36ebefb7e89a6ddbcac9086d2daf179ed10c
ErildoJS <erildofranciscojs@gmail.com>
2024-01-25 12:27:33 +0100

    add await in list

--- src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController.ts
+++ src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController.ts
@@ -6,7 +6,7 @@ class ListRentalsByUserController {
     async handle(request: Request, response: Response): Promise<Response> {
         const {id} = request.user
         const listRentalsByUserUseCase = container.resolve(ListRentalsByUserUseCase)
-        const rentals = listRentalsByUserUseCase.execute(id)
+        const rentals = await listRentalsByUserUseCase.execute(id)
         return response.json(rentals)
     }
 }

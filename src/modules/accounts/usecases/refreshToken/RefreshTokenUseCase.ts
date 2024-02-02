d79ece3f0e2ceee79c57cd96e146ed4fcec73e66
ErildoJS <erildofranciscojs@gmail.com>
2024-02-02 22:13:27 +0100

    refactor: fix refresh_token

--- src/modules/accounts/usecases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/usecases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
     email: string
 }
 
+interface ITokenResponse {
+    token: string,
+    refresh_token: string
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
         private dayjsDateProvider: IDateProvider
     ) {}
     
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const {sub, email} = verify(token, auth.secret_refresh_token) as IPayload
         const user_id = sub
         const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(user_id, token)
@@ -35,7 +40,15 @@ class RefreshTokenUseCase {
             refresh_token,
             user_id
         })
-        return refresh_token
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
+
+        return {
+            refresh_token,
+            token: newToken
+        }
 
     }
 }
